3abcf67d28770bca14b5a5ba84251902c20d9857
Redomine <romankaev@gmail.com>
2020-05-03 01:08:11 +0300

    update
    Removed "hello world", added first raw html code.

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#def show(request):
+# #    return HttpResponse("Firs try")
+
 def show(request):
-    return HttpResponse("Firs try")
+    return render(request, 'comment_section/comments.html')
