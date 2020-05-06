f3e5ed2c4faf8d1d672e1eca1b1acd1ff96cb6de
Redomine <romankaev@gmail.com>
2020-05-07 00:55:50 +0300

    update
    Spelling problems fix

--- testboard/asgi.py
+++ testboard/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for testbord project.
+ASGI config for testboard project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbord.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testboard.settings')
 
 application = get_asgi_application()
--- testboard/wsgi.py
+++ testboard/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for testbord project.
+WSGI config for testboard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbord.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testboard.settings')
 
 application = get_wsgi_application()
