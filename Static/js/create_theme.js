77bf481db36dc1a3032f6711bcc387ebae4b7961
Redomine <romankaev@gmail.com>
2020-05-07 07:39:44 +0300

    update
    Fixed broken create_theme.js

--- Static/js/create_theme.js
+++ Static/js/create_theme.js
@@ -1,11 +1,14 @@
 let themeButton = document.querySelector('.theme_button');
-let postForm = document.querySelector('post-form');
+let postForm = document.querySelector('.post-form');
+
 
 themeButton.onclick = function() {
+
   console.log(typeof(postForm));
-  postForm.classList.add('hide');
   if (themeButton.textContent === 'Закрыть форму постинга') {
-  themeButton.textContent = 'Создать тред'} else {
-  themeButton.textContent = 'Закрыть форму постинга'}
+  themeButton.textContent = 'Создать тред'
+  postForm.classList.add('hide');} else {
+  themeButton.textContent = 'Закрыть форму постинга'
+  postForm.classList.remove('hide')}
 
 };
