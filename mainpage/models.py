7d5d091322877af785bf9ce636db2796e52356ca
Redomine <romankaev@gmail.com>
2020-05-09 23:08:03 +0300

    update
    submission form finished
    
    first db model

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Posts(models.Model):
+    name = models.CharField("Имя треда", max_length = 150)
+    description = models.TextField("Тело треда",  max_length = 15000)
+    url = models.SlugField(max_length=160)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Тред"
+        verbose_name_plural = "Треды"
