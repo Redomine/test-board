0c40b9fd96a1c6b70cb6627b4404f4bf37f5148d
Redomine <romankaev@gmail.com>
2020-05-12 17:24:21 +0300

    update
    It's working with mysql now

--- testboard/settings.py
+++ testboard/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'testboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'board_data',
+        'USER': 'root',
+        'PASSWORD': '7M8Eh56d',
+        'HOST': 'localhost',
     }
 }
 
