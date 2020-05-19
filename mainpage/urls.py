8bbcc87843e3d4b0943aef1539ddee62f2b50297
Redomine <romankaev@gmail.com>
2020-05-19 16:37:22 +0300

    update
    trying to automate url generation

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.show_main_page.as_view()),
     path("adding/", views.AddPost.as_view(), name="add_post"),
-    path("<int:id>/", views.show_post.as_view())
+    path("<int:key>/", views.show_post.as_view())
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -10,21 +10,33 @@ def show(request):
     return render(request, 'main_page/main_page.html')
 
 
+
+
+
+
 class show_main_page(View):
     def get(self, request):
         treds = Post.objects.all()
         return render(request, "main_page/main_page.html", {"tred_list": treds})
 
 class show_post(View):
-    def get(self, request, id):
-        return render(request, "comment_section/comments.html")
+    def get(self, request, key):
+        tred = Post.objects.get(url=key)
+        return render(request, "comment_section/comments.html", {"tred": tred})
 
 
 class AddPost(View):
     def post(self, request):
+        treds = Post.objects.all()
+        for tred in treds:
+            last_url = tred.url
+        new_url = str(int(last_url)+1)
+
         form = post_form(request.POST)
         if form.is_valid():
             form.save()
-        else:
-            print(form)
+
+        for tred in treds:
+            print(tred.url)
+        tred.url = new_url
         return redirect("/")
