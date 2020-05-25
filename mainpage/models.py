7507c4dd72936d6c99d8f4c83227396a35cbf2b5
Redomine <romankaev@gmail.com>
2020-05-25 16:18:05 +0300

    update
    new comment model

--- mainpage/models.py
+++ mainpage/models.py
@@ -27,9 +27,15 @@ class Tred(models.Model):
 
 class Comment(models.Model):
 
-    tred_parent = models.ForeignKey(Tred, on_delete=models.CASCADE)
+    tred_parent = models.ForeignKey(Tred, verbose_name="Родительский тред", on_delete=models.CASCADE)
+    comment_parent = models.ForeignKey(
+        'self', verbose_name="Родительский комментарий", on_delete=models.SET_NULL, blank=True, null=True
+    )
     comment_content = models.TextField("Тело комментария",  max_length = 15000)
     comment_date = models.DateField("Дата создания", default = datetime.datetime.utcnow().replace(tzinfo=utc))
+
+    def __str__(self):
+        return self.comment_content[0:200]
 
     class Meta:
         verbose_name = "Комментарий"
